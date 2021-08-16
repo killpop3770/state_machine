978c77d70736ea85534a079ffe23204864c00daa
killpop3770 <Kfgjxrf345%>
2021-08-16 15:35:57 +0300

    delete useless tests in main, delete old function in state_machine_class

--- main.cpp
+++ main.cpp
@@ -184,25 +184,6 @@ public:
         return false;
     }
 
-//    bool setField(Field *field, Type type, const char *data) {
-//        assert(field != nullptr);
-//
-//        if (not m_record->isDir and not beginState) {
-//            field->type = type;
-//            field->size = normalizeLen(data, 127);
-//            memset(field->data, 0, 128);
-//            strncpy(field->data, data, field->size);
-//
-//            field->next = nullptr; // присвоение адреса предыдущему полю
-//            if (m_lastField)
-//                m_lastField->next = field;
-//
-//            m_lastField = field;
-//            return true;
-//        }
-//        return false;
-//    } <--deprecated setField version!
-
     bool end(bool commit) {
 
         if (not m_record and not beginState)
@@ -230,7 +211,6 @@ public:
         beginState = true;
         return true;
     }
-
 
     bool readRecord(uint16_t id) {
 
@@ -292,26 +272,9 @@ public:
         ser.serialize(mag0);
     }
 
-
     ////////////// Only for tests ////////////
-    [[nodiscard]] size_t getIndex() const {
-        return index;
-    }
 
     Record *getRecord() { return m_record; }
-
-    void resetM_Record() {
-
-        m_record->id = 0;
-        m_record->pid = 0;
-        m_record->isDir = false;
-        m_record->lastUpdate = 0;
-        m_record->count = 0;
-        m_record->type = 0;
-        memset(m_record->fields->data, 0, 128);
-        memset(m_record->fields->next->data, 0, 128);
-        memset(m_record->fields->next->next->data, 0, 128);
-    }
     ////////////// Only for tests ////////////
 
 private:
@@ -328,106 +291,6 @@ private:
 
 
 /////////////////////////////////////////// Test methods ///////////////////////////////////////////////////////////////
-//static size_t cnt_for_test = 0; // only for test functions
-//
-//void printRecord(StateMachine sm, uint16_t id) {
-//    uint16_t len0 = 0;
-//    const char *type_name[3] = {"URL", "PASSWORD", "LOGIN"};
-//
-//    sm.readRow(id);
-//    Record *rec = sm.getRecord();
-//    len0 = strlen(rec->name);
-//
-//    std::cout << "==========================================" << std::endl;
-//    std::cout << "id:         " << rec->id << std::endl;
-//    std::cout << "pid:        " << rec->pid << std::endl;
-//    std::cout << "isDir:      " << std::boolalpha << rec->isDir << std::endl;
-//    std::cout << "type:       " << (int) rec->type << std::endl;
-//    std::cout << "count:      " << (int) rec->count << std::endl;
-//    std::cout << "lastUpdate: " << rec->lastUpdate << std::endl;
-//    std::cout << "name:       ";
-//    for (int j = 0; j < (int) len0; j++)
-//        printf("%c", rec->name[j]);
-//    std::cout << std::endl;
-//    std::cout << "==========================================" << std::endl;
-//
-//    Field *pField = rec->fields;
-//    int i = 0;
-//    while (i < rec->count) {
-//        std::cout << "Field #" << std::endl;
-//        std::cout << "Data: ";
-//        for (int j = 0; j < (int) pField->size; j++)
-//            printf("%c", pField->data[j]);
-//        std::cout << std::endl;
-//        std::cout << "Size: " << (int) pField->size << std::endl;
-//        std::cout << "Type: " << type_name[pField->type] << std::endl;
-//        std::cout << "Next: " << pField->next << std::endl;
-//        std::cout << std::endl;
-//        pField = pField->next;
-//        i++;
-//    }
-//}
-//
-//void create_update_func_test(StateMachine &sm, Record *row) {
-//    std::cout << "TEST " << cnt_for_test << std::endl;
-//    sm.createUpdateRow(row);
-//    cnt_for_test++;
-//}
-//
-//void begin_func_test(StateMachine sm, Record *newRow) {
-//    sm.begin(newRow);
-//    if (not sm.begin(newRow)) {
-//        std::cout << "==========================================" << std::endl;
-//        std::cout << "begin_func_test:  It's ok!" << std::endl;
-//    } else {
-//        std::cout << "==========================================" << std::endl;
-//        std::cout << "begin_func_test:  Fault!" << std::endl;
-//    }
-//}
-//
-//void setFields_func_test(StateMachine sm, Record *newRow) {
-////    sm.setField(&newRow->fields[URL], URL, "some_data");
-////    sm.setField(&newRow->fields[URL], URL, "some_data");
-////    sm.setBeginState(true);
-////    if (not sm.setField(&newRow->fields[URL], URL, "some_data")) {
-////        std::cout << "==========================================" << std::endl;
-////        std::cout << "setFields_func_test: It's ok!" << std::endl;
-////    } else {
-////        std::cout << "==========================================" << std::endl;
-////        std::cout << "setFields_func_test: Fault!" << std::endl;
-////    }
-//}
-//
-//
-//void test_create(Serializer *ser, int i) {
-//    Record row;
-//
-//    row.id = i + 6; // id > 0
-//    row.pid = 400 + i;
-//    row.isDir = false;
-//    row.lastUpdate = 2009 + i;
-//    row.count = 3;
-//
-//    const char *arr[] = {"https://test.com", "my_password", "my_login"};
-//    createField(&row, arr);
-//    initRow(row.name, "my_name");
-//
-//    ser->serialize(mag);
-//    ConfigSectionDescriptor<Record>::pack(*ser, row);
-//}
-//
-//void findRow_func_test(StateMachine &sm, uint16_t id) {
-////    bool *pBlock;
-//////    char *record0 = sm.getRecord();
-////    findBlock(id, pBlock);
-////    if (not isNewRow) {
-////        std::cout << "==========================================" << std::endl;
-////        std::cout << "findRow_func_test:    Fault!" << std::endl;
-////    } else {
-////        std::cout << "==========================================" << std::endl;
-////        std::cout << "findRow_func_test:    It's ok!" << std::endl;
-////    }
-//} <-- deprecated tests version
 
 void initRow(char *_ptr, const char *_s) {
 
@@ -480,68 +343,6 @@ void createField(Record *row) {
 /////////////////////////////////////////// Test methods ///////////////////////////////////////////////////////////////
 
 //int main() {
-//
-////     Initialize db
-//    for (int i = 0; i < 10; i++) {
-//        Serializer ser(database[i], row_size);
-//        test_create(&ser, i);
-//    }
-//
-//    // Data for tests
-//    Record row0{15, 1111, false, 2020, 4};
-//    initRow(row0.name, "Ivan");
-//    const char *arr[] = {"https://github.com", "password_pid_1234", "login_pid_1234", "sone"};
-//    createField(&row0, arr);
-//
-//    Record row1{16, 1111, false, 2020, 4};
-//    initRow(row1.name, "AlexAlexAlexAlexAlexAlexAlexAlexAlexAlexAlexAlexAlexAlexAlexAlexAlexAlexAlexAlexAlexAlexAlexAlexAlexAlexAlexAlexAlexAlexAlexAlex");
-//    const char *arr1[] = {"https://buhtig.moc", "password_pid_1234", "login_pid_1234", "diem"};
-//    createField(&row1, arr1);
-//
-//    Record row2{16, 1000, false, 2000, 3};
-//    initRow(row2.name, "Rick");
-//    const char *arr2[] = {"https://buhtig.io", "password_1234", "login_1234"};
-//    createField(&row2, arr2);
-//
-//    Record row3{16, 1010, false, 2010, 3};
-//    initRow(row3.name, "Nick");
-//    const char *arr3[] = {"https://asdffdsaasdf.com/index", "pass_pass_pass_pass", ""};
-//    createField(&row3, arr3);
-//
-//    Record row4{16, 1011, true, 2012, 3};
-//    initRow(row4.name, "Gick");
-//    const char *arr4[] = {"https://asdffdsaasdffdsaasdf.com/asdffdsaasdffdsaindex.html", "pass_pass_pass_pass_pass_pass_pass_pass_pass_pass_pass_pass", "login"};
-//    createField(&row4, arr4);
-//
-//    Record row5{16, 100, false, 1000, 3};
-//    initRow(row5.name, "Alex0");
-//    const char *arr5[] = {"https://n&tw0rk.c0m", "pass0", "log0"};
-//    createField(&row5, arr5);
-//
-//    StateMachine sm;
-//
-//    ///////////////// TESTs ///////////////////
-//    create_update_func_test(sm, &row0);
-//    sm.resetM_Record();
-//    printRecord(sm,15);
-//    create_update_func_test(sm, &row1);
-////    printRecord(sm,16);
-////    create_update_func_test(sm, &row2);
-////    printRecord(sm,16);
-////    create_update_func_test(sm, &row3);
-////    printRecord(sm,16);
-////    create_update_func_test(sm, &row4);
-////    printRecord(sm,16);
-////    create_update_func_test(sm, &row5);
-////    printRecord(sm,16);
-//
-////
-////    begin_func_test(sm, &row1);
-////
-////    setFields_func_test(sm, &row6);
-////
-////    findRow_func_test(sm, 17);
-//    ///////////////////////////////////////////
-//
+
 //    return 0;
-//} <-- deprecated main func
+//}
